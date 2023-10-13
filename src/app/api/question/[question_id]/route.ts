ac98ac644d039d3fda139956e5fffb44726154ed
nkita <nkitam7@gmail.com>
2023-10-13 23:14:31 +0900

    bug fix.

--- src/app/api/question/[question_id]/route.ts
+++ src/app/api/question/[question_id]/route.ts
@@ -33,30 +33,5 @@ export async function GET(request: Request, { params }: { params: any }) {
     return responseJson(200, response)
 }
 
-/**
- * Questionの作成。
- * @param quizId クイズIDが渡された場合はそのまま作成
- * @returns question JSON Object（新規作成されたQuestionを返却する）
- */
-export async function POST(request: Request) {
-    if (request.headers.get('content-type') !== 'application/json') return responseJson(400)
 
-    const session = await getServerSession(nextAuthOptions)
-    const uid = session ? (!session.user.id) ? guestId() : session.user.id : guestId()
-
-    // パラメータからquizIDを取得
-    const res = await request.json()
-    // ランダムまたは、ID指定でQuizを取得
-    let quiz: any
-    if (!res.quizId) {
-        quiz = await rand_select_one()
-    } else {
-        if (!isUUID(res.quizId)) return responseJson(404)
-        quiz = await select({ id: res.quizId })
-    }
-    //Questionを作成
-    const result = await create(uid, quiz[0].id)
-
-    return responseJson(200, result)
-}
-
+export const POST = async () => responseJson(405)
